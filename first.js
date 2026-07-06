e7e7fe572594bede8566572dbb635de39595d312
vivek-prajapat560 <vp3422436@gmail.com>
2026-07-06 19:02:47 +0530

    First commit

--- first.js
+++ first.js
@@ -1,3 +1,6 @@
+let userScore = 0;
+let computerScore = 0;
+let drawScore = 0;
 function playGame(userChoice){
   const choice = ["Rock","Paper","Scissors"];
   const computerChoice = 
@@ -25,4 +28,29 @@ function playGame(userChoice){
       `You: ${userChoice}<br>
       Computer: ${computerChoice}<br>
       ${result}`;
+}
+if(user === computer){
+    drawScore++;
+    document.getElementById("drawScore").innerText = drawScore;
+}
+else if(
+(user==="Rock" && computer==="Scissors") ||
+(user==="Paper" && computer==="Rock") ||
+(user==="Scissors" && computer==="Paper")
+){
+    userScore++;
+    document.getElementById("userScore").innerText = userScore;
+}
+else{
+    computerScore++;
+    document.getElementById("computerScore").innerText = computerScore;
+}
+function resetGame(){
+    userScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+
+    document.getElementById("userScore").innerText = 0;
+    document.getElementById("computerScore").innerText = 0;
+    document.getElementById("drawScore").innerText = 0;
 }
